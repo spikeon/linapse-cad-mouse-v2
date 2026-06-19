a267d02743590a5d381d506a26dd9210aa83eee3
spikeon <spikeon@gmail.com>
2026-06-19 01:19:36 -0400

    docs(version): update userscript and installer badges version to 2.6.0

--- linux/linapse-browser-connector.user.js
+++ linux/linapse-browser-connector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Linapse Browser Connector
 // @namespace    https://github.com/sb-ocr/cad-mouse-mk2
-// @version      2.5.4
+// @version      2.6.0
 // @description  Makes OnShape and SketchUp Web connect to the local spacenav-ws bridge on Linux
 // @author       CAD Mouse MK2 contributors
 // @match        https://cad.onshape.com/*
